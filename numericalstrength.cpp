ae96c84f821dc198d3ff45ef07ce007ffba9facb
dusiX <102617702+dusiX@users.noreply.github.com>
2022-07-10 00:02:22 +0200

    Update and rename CPP2.cpp to numericalstrength.cpp

--- numericalstrength.cpp
+++ numericalstrength.cpp
@@ -1,10 +1,12 @@
+// Count a numerical strength of provided number
+
 #include <iostream>
 using namespace std;
 
 int main() {
 	unsigned int N;
 	unsigned int silnia = 1;
-	cout << "WprowadŸ liczbê N: \n";
+	cout << "Provide N: \n";
 	cin >> N;
 
 	for (int i = 1; i <= N; i++)
